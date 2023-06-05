ce19f5201f8a4e65861e4896bab24200d91825c8
Jens Krumsieck <jenso666@live.de>
2023-06-05 13:51:08 +0200

    fix

--- avogrado_plot_tsv.py
+++ avogrado_plot_tsv.py
@@ -12,13 +12,18 @@ parser.add_argument("files", help="specify csv files", nargs='+')
 parser.add_argument("-b", "--bars", help="bars")
 parser.add_argument("-n", "--nonorm", action='store_true',  help="do not normalize")
 parser.add_argument("-l", "--labels", nargs='+',  help="labels")
+parser.add_argument("-s", "--shift", help="shift by cm-1")
 args = parser.parse_args()
 
 ### END ARGPARSE ###
 
 
 def getMax(y: pd.Series):
-    return y.max()
+    idx = y[x.lt(250)].index[0]
+    if idx == 0:
+        idx = y.shape[0]
+    max = y.iloc[:idx].max()
+    return max
 
 
 # Paths
@@ -28,19 +33,28 @@ bars = args.bars
 hasLabels = labels != None
 normalize = not args.nonorm
 df = pd.DataFrame()
+pos = 0
+i=0
 for file in paths:
     filename, file_extension = os.path.splitext(file)
     if(file_extension == ".tsv"):
+        pos = i
         df = pd.concat([df, (pd.read_csv(file, header=1, usecols=[0, 1], encoding='latin-1', sep="\t"))], axis=1)
     if(file_extension == ".csv"):
         df = pd.concat([df, (pd.read_csv(file, header=1, usecols=[0, 1], encoding='latin-1'))], axis=1)
+    i+=1
 
 num_spc = int(df.shape[1])
 gmax = 0  # global max
 fig, ax = plt.subplots()
 for i in range(0, num_spc, 2):
-    j = int(i/2)
+    j = int(i/2)        
     x = df.iloc[:, i]
+    if j == pos and args.shift != None and args.shift != 0: # last tsv index
+        print("shifting tsv with id " + str(j) + " by " + str(args.shift))
+        x = 10000000/x
+        x = x - float(args.shift)
+        x = 10000000/x
     y = df.iloc[:, i+1]
     if normalize:
         # normalize
@@ -51,9 +65,9 @@ for i in range(0, num_spc, 2):
     if(max > gmax):
         gmax = max
     if(not hasLabels or j >= len(labels)):
-        plt.plot(x, y, colors[j])
+        plt.plot(x, y, colors[j], linewidth=1.5)
     else:
-        plt.plot(x, y, colors[j], label=labels[j])
+        plt.plot(x, y, colors[j], label=labels[j], linewidth=1.5)
 
 if bars != None:
     bdf = pd.read_csv(bars, header=1, usecols=[0, 1], encoding="latin-1", sep="\t")
@@ -61,6 +75,6 @@ if bars != None:
     y = bdf.iloc[:, 1]
     plt.bar(x, y / y.max(), 3)
 applyAxisSettings(ax, gmax, normalize)
-
+ax.legend(fontsize=10)
 plt.savefig("out/img.png", dpi=1200)
 plt.savefig("out/img.svg", dpi=1200)
